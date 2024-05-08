d3b18e5fac35cb60d6769055589477ca944e9a31
ctrellu <56796854+ctrellu@users.noreply.github.com>
2024-05-08 17:19:37 +0200

    Update setup_tracking.sh

--- setup_tracking.sh
+++ setup_tracking.sh
@@ -27,5 +27,15 @@ cd ~/Desktop/squid_tracking/software/drivers\ and\ libraries/daheng\ camera/Gala
 python3 setup.py build
 sudo python3 setup.py install
 
+# install CUDA
+cd ~/Downloads
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get -y install cuda
+pip install cuda-python
+pip install cupy-cuda12x
+pip3 install torch torchvision torchaudio
+
 # enable access to serial ports without sudo
 sudo usermod -aG dialout $USER
