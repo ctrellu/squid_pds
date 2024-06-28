46f28d4735f51b897d54064ae138832fc1b80973
ctrellu <56796854+ctrellu@users.noreply.github.com>
2024-06-28 17:08:40 +0200

    Update setup_fluo.sh

--- setup_fluo.sh
+++ setup_fluo.sh
@@ -12,7 +12,7 @@ sudo apt install python3-pyqt5.qtsvg
 sudo apt-get install git -y
 cd ~/Desktop
 git clone https://github.com/livingpatterns/squid_imaging.git
-cd squid_fluo/software
+cd squid_imaging/software
 mkdir cache
 
 # install libraries 
@@ -20,9 +20,9 @@ pip3 install qtpy pyserial pandas imageio crc==1.3.0 lxml numpy tifffile scipy n
 pip3 install opencv-python-headless opencv-contrib-python-headless
 
 # install camera drivers
-cd ~/Desktop/squid_fluo/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux-x86_Gige-U3_32bits-64bits_1.2.1911.9122
+cd ~/Desktop/squid_imaging/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux-x86_Gige-U3_32bits-64bits_1.2.1911.9122
 ./Galaxy_camera.run
-cd ~/Desktop/squid_fluo/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux_Python_1.0.1905.9081/api
+cd ~/Desktop/squid_imaging/software/drivers\ and\ libraries/daheng\ camera/Galaxy_Linux_Python_1.0.1905.9081/api
 python3 setup.py build
 sudo python3 setup.py install
 
